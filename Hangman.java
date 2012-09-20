fd0eeb9a128ba987be655d8e4ad5a9a6bf871b5a
m2m tech <tech@m2m.at>
2012-09-20 15:34:19 +0200

    assignment #4 task #2

--- Hangman.java
+++ Hangman.java
@@ -13,27 +13,39 @@ import java.awt.*;
 
 
 public class Hangman extends ConsoleProgram {
-	
+
 	private String wordToGuess;
 	private String currentWord;
 
-    public void run() {
+	private HangmanCanvas canvas;
+
+	public void init() {
+		canvas = new HangmanCanvas();
+		add(canvas);
+	}
+
+	public void run() {
 		println("Welcome to Hangman!");
-		
+
 		HangmanLexicon lexicon = new HangmanLexicon();
 		RandomGenerator rgen = RandomGenerator.getInstance();
-		
+
 		wordToGuess = lexicon.getWord(rgen.nextInt(lexicon.getWordCount()));
 		currentWord = dashifyWord();
-		int numberOfGuesesLeft = 8;
-
 		boolean gameWon = false;
+		int numberOfGuesesLeft = 8;
+		canvas.reset();
+		
+
 		while (!gameWon && (numberOfGuesesLeft > 0)) {
+			canvas.displayWord(currentWord);
 			println("The word now looks like this: " + currentWord);
 			println("You have " + numberOfGuesesLeft + " guesses left.");
 			String guessInput = readLine("Your guess: ");
-			char guess = guessInput.charAt(0);
-			if ((guessInput.length() > 1) || !Character.isLetter(guess)) {
+			char guess;
+			if ((guessInput.length() == 0) 
+					|| (guessInput.length() > 1) 
+					|| !Character.isLetter(guess = guessInput.charAt(0))) {
 				println("Please enter single letters only!");
 				continue;			
 			}
@@ -43,46 +55,48 @@ public class Hangman extends ConsoleProgram {
 					gameWon = true;
 				}
 			} else  {
+				canvas.noteIncorrectGuess(guess);
 				println("There are no " + guess + "'s in the word.");
 				numberOfGuesesLeft--;
 			}						
 		}
-		
+
 		if (gameWon) {
 			println("You guessed the word: " + wordToGuess);
 			println("You win.");			
+			canvas.displayWord(currentWord);
 		} else {
 			println("You are completely hung.");
 			println("The word was: " + wordToGuess);
 			println("You lose.");
 		}
-			
+
 	}
-    
-    private boolean guessCorrect(char guess) {
-    	guess = Character.toUpperCase(guess);
-    	boolean guessCorrect = false;
-    	String newWord = "";
-    	for (int i = 0; i < wordToGuess.length(); i++) {
-    		if (wordToGuess.charAt(i) == guess) {
-    			newWord += guess;
-    			guessCorrect = true;
-    		} else {
-    			newWord += currentWord.charAt(i);
-    		}
-    	}
-    	if (guessCorrect) {
-    		currentWord = new String(newWord);
-    	}
-    	return guessCorrect;
-    }
-    
-    private String dashifyWord() {
-    	String result = "";
-    	for(int i = 0 ; i < wordToGuess.length(); i++) {
-    	      result += "-";
-    	   }
-    	   return result;
-    }
+
+	private boolean guessCorrect(char guess) {
+		guess = Character.toUpperCase(guess);
+		boolean guessCorrect = false;
+		String newWord = "";
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			if (wordToGuess.charAt(i) == guess) {
+				newWord += guess;
+				guessCorrect = true;
+			} else {
+				newWord += currentWord.charAt(i);
+			}
+		}
+		if (guessCorrect) {
+			currentWord = new String(newWord);
+		}
+		return guessCorrect;
+	}
+
+	private String dashifyWord() {
+		String result = "";
+		for(int i = 0 ; i < wordToGuess.length(); i++) {
+			result += "-";
+		}
+		return result;
+	}
 
 }
--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -4,35 +4,169 @@
  * This file keeps track of the Hangman display.
  */
 
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+import sun.misc.Cleaner;
+
 import acm.graphics.*;
 
 public class HangmanCanvas extends GCanvas {
 
-/** Resets the display so that only the scaffold appears */
+	private double xHangman, yHangman, yBody, yLeg;
+	private int numberOfWrongGuesses;
+	private GLabel wrongGuesses, currentWord;
+
+	/** Resets the display so that only the scaffold appears */
 	public void reset() {
-		/* You fill this in */
+		removeAll();
+		numberOfWrongGuesses = 0;
+		xHangman = getWidth() / 2;
+		yHangman = (getHeight() - SCAFFOLD_HEIGHT) * 0.25 + ROPE_LENGTH;
+		GPen scaffold = new GPen(xHangman, yHangman);
+		scaffold.drawLine(0, -ROPE_LENGTH);
+		scaffold.drawLine(-BEAM_LENGTH, 0);
+		scaffold.drawLine(0, SCAFFOLD_HEIGHT);
+		add(scaffold);
+		
+		double xLabels = xHangman - BEAM_LENGTH;
+		double yLabels = yHangman + SCAFFOLD_HEIGHT;
+		currentWord = new GLabel("");
+		currentWord.setLocation(xLabels, yLabels + HEAD_RADIUS / 2);
+		currentWord.setFont("SansSerif-28");
+		add(currentWord);
+		wrongGuesses = new GLabel("");
+		wrongGuesses.setLocation(xLabels, yLabels + HEAD_RADIUS);
+		add(wrongGuesses);
 	}
 
-/**
- * Updates the word on the screen to correspond to the current
- * state of the game.  The argument string shows what letters have
- * been guessed so far; unguessed letters are indicated by hyphens.
- */
+	/**
+	 * draw head & set y position where the body begins
+	 */
+	private void drawHead() {
+		double d = 2 * HEAD_RADIUS;
+		GOval head = new GOval(xHangman - HEAD_RADIUS, yHangman, d, d);
+		yBody = yHangman + d;
+		add(head);		
+	}
+
+	/**
+	 * draw body and set y position where the legs begin
+	 */
+	private void drawBody() {
+		yLeg = yBody + BODY_LENGTH;
+		GLine body = new GLine(xHangman, yBody, xHangman, yLeg);
+		add(body);		
+	}
+
+	/**
+	 * draw left (-1) or right (1) arm
+	 * @param side
+	 */
+	private void drawArm(int side) {
+		GPen arm = new GPen(xHangman, yBody + ARM_OFFSET_FROM_HEAD);
+		arm.drawLine(side * UPPER_ARM_LENGTH, 0);
+		arm.drawLine(0, LOWER_ARM_LENGTH);
+		add(arm);
+	}
+
+	/**
+	 * draw left (-1) or right (1) leg
+	 * @param side
+	 */
+	private void drawLeg(int side) {
+		GPen leg = new GPen(xHangman, yLeg);
+		leg.drawLine(side * HIP_WIDTH, 0);
+		leg.drawLine(0, LEG_LENGTH);
+		add(leg);
+	}
+
+	/**
+	 * draw left (-1) or right (1) foot
+	 * @param side
+	 */
+	private void drawFoot(int side) {
+		double y = yLeg + LEG_LENGTH;
+		double x = xHangman + side * HIP_WIDTH;
+		GLine foot = new GLine(x, y, x + side * FOOT_LENGTH, y);
+		add(foot);
+	}
+
+	/**
+	 * Updates the word on the screen to correspond to the current
+	 * state of the game.  The argument string shows what letters have
+	 * been guessed so far; unguessed letters are indicated by hyphens.
+	 */
 	public void displayWord(String word) {
-		/* You fill this in */
+		currentWord.setLabel(word);	
 	}
 
-/**
- * Updates the display to correspond to an incorrect guess by the
- * user.  Calling this method causes the next body part to appear
- * on the scaffold and adds the letter to the list of incorrect
- * guesses that appears at the bottom of the window.
- */
+	/**
+	 * Updates the display to correspond to an incorrect guess by the
+	 * user.  Calling this method causes the next body part to appear
+	 * on the scaffold and adds the letter to the list of incorrect
+	 * guesses that appears at the bottom of the window.
+	 */
 	public void noteIncorrectGuess(char letter) {
-		/* You fill this in */
+		numberOfWrongGuesses++;
+		switch (numberOfWrongGuesses) {
+		case 1:
+			drawHead();
+			break;
+		case 2:
+			drawBody();
+			break;
+		case 3:
+			drawArm(-1);
+			break;
+		case 4:
+			drawArm(1);
+			break;
+		case 5:
+			drawLeg(-1);
+			break;
+		case 6:
+			drawLeg(1);
+			break;
+		case 7:
+			drawFoot(-1);
+			break;
+		case 8:
+			drawFoot(1);
+			break;
+		default:
+			break;
+		}
+		
+		updateWrongGuesses(letter);
+	}
+	
+	private void updateWrongGuesses(char letter) {
+		letter = Character.toUpperCase(letter);
+		String newString = "";
+		String oldString = wrongGuesses.getLabel();
+		int length = oldString.length();
+		boolean letterAdded = false;
+		for (int i = 0; i < length; i++) {
+			char ch = oldString.charAt(i);
+			if (ch < letter) {
+				newString += ch;
+			} else {
+				if (ch > letter) {
+					newString += letter;
+				}
+				newString += oldString.substring(i);
+				letterAdded = true;
+				break;
+			}
+		}
+		if (!letterAdded) {
+			newString += letter;
+		}		
+		wrongGuesses.setLabel(newString);
 	}
 
-/* Constants for the simple version of the picture (in pixels) */
+	/* Constants for the simple version of the picture (in pixels) */
 	private static final int SCAFFOLD_HEIGHT = 360;
 	private static final int BEAM_LENGTH = 144;
 	private static final int ROPE_LENGTH = 18;
